7de3af0152159213025a3b8ef8602beca47eb49d
Kathan Agarwal <kathanhagarwal@gmail.com>
2025-01-15 17:34:01 +0530

    Basic Structure

--- index.php
+++ index.php
@@ -3,9 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Simple Chat Application</title>
+    <title>Chat Application</title>
+    <link rel="stylesheet" href="style.css"
 </head>
 <body>
-    
+    <div id="wrapper">
+        <div id="menu">M E N U</div>
+        <div id="chatbox"> C h a t b o x</div>
+        <form name="message" action="">
+            <input name="usermsg" type="text" id="usermsg"></input>
+            <input name="submitmsg" type="submit" id="submit" value="Send"></input>
+</form>
+
+
+    </div>
 </body>
 </html>
